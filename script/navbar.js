687479b45fd2a14d7eedbb334cd0794a9b54e9ab
ValentinBeaussart <valentin.beaussart@gmail.com>
2024-04-05 03:43:35 +0200

    navbar

---
+++ script/navbar.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+  
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scroll');
+    } else {
+      navbar.classList.remove('navbar-scroll');
+    }
+  });
+  
